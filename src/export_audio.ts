e0de367273bb61d4937c017eb5b9fe46112fab70
咸光金 <guangjin2.xian@zhaopin.com.cn>
2024-04-23 16:13:15 +0800

    'update'

--- src/export_audio.ts
+++ src/export_audio.ts
@@ -1,4 +1,5 @@
-import { Action, ActionProps, TTS, res, showToast } from "@enconvo/api";
+import { PlayPoolItem } from "@enconvo/api";
+import { Action, ActionProps, ServiceProvider, TTS, isTextFile, res, showToast, uuid } from "@enconvo/api";
 import { Exporter, Clipboard } from "@enconvo/api";
 
 
@@ -7,7 +8,39 @@ export default async function main(req: Request) {
   const { options } = await req.json()
   const { text, context } = options
 
-  let content = text || context || await Clipboard.selectedText();
+
+  let filePaths: string[] = options.draggedContext || []
+  let saveName: string | null = null
+
+  const textFilePaths = filePaths.filter((filePath) => {
+    return isTextFile(filePath)
+  }).map((filePath) => {
+    return {
+      id: uuid(),
+      filePath,
+      type: "file"
+    }
+  })
+  let docContent = null
+  if (textFilePaths.length > 0) {
+    // 第一个文件的文件名
+    saveName = `${textFilePaths[0].filePath.split("/").pop()}.mp3` || "-audio.mp3"
+
+    const loader: any = await ServiceProvider.load({
+      extensionName: "chat_with_doc",
+      commandName: "load_docs"
+    })
+    const docs: any[] = await loader.load({ docs: textFilePaths })
+    docContent = docs.map((doc: any) => {
+      return doc.pageContent
+    }).join("\n\n")
+  }
+
+
+  let content = docContent || text || context || await Clipboard.selectedText();
+  if (!saveName) {
+    saveName = content.split(' ')[0] + ".mp3"
+  }
 
   if (!content) {
     throw new Error("No content to export")
@@ -15,7 +48,7 @@ export default async function main(req: Request) {
 
   // save  content to markdown file
   const path = await Exporter.showSavePanel({
-    nameFieldStringValue: "audio.mp3",
+    nameFieldStringValue: saveName,
   })
 
 
@@ -23,15 +56,21 @@ export default async function main(req: Request) {
     throw new Error("No path selected")
   }
 
-  console.log("path", path)
   res.write({
     content: `Saving audio to "${path}"`,
     overwrite: true,
     loading: true
   })
-  await new Promise(resolve => setTimeout(resolve, 5000000));
 
-  await TTS.ttsToFile({ text: content, outputFile: path, ttsOptions: options.tts_providers })
+  await TTS.ttsToFile({
+    text: content, concurrency: options.concurrency, outputFile: path, chunkSize: 1000, ttsOptions: options.tts_providers, playCallBack: async (item: PlayPoolItem, progress?: number) => {
+      await res.write({
+        content: `Progress: ${progress}%`,
+        overwrite: true,
+        loading: true
+      })
+    }
+  })
 
   const actions: ActionProps[] = [
     Action.ShowInFinder({ path: path }),
